e5478cd544423301f3e5ec03076d9e7edd220d15
rafid-dev <vorahsan@gmail.com>
2024-01-24 17:36:13 +0600

    Statuses

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
+  bot.editStatus(process.env.status, {name: process.env.statusName, type: process.env.statusType})
 });
 
 bot.connect(); // Get the bot to connect to Discord
